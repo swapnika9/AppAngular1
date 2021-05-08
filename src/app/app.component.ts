2195cb25dcabdbad864abd1d1cdc0884eb2a1cae
swapnika <telagathotiswapnika9@gmail.com>
2021-05-08 20:12:15 +0530

    Project Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AppAngular1';
+}
+  
+
+
+
+
+
+
+
+
+
+
+  
+  // item:string;
+
+  // todoItems:string[]=[];
+  
+
+  // //to add item
+  // addItem(){
+  //   //push item into array
+  //   this.todoItems.push(this.item);
+  //   this.item='';
+   
+  // }
+
+  // //to delete item
+  // deleteItem(index){
+  //     this.todoItems.splice(index,1)
+  //}
+  // list:string[]=[];
+  // sendItem(item){
+  //   this.list.push(item)
+  // }
+  // status:boolean=false;
+  // changeStatus(){
+  //    this.status=!this.status;
+  //  }
+//}
+
+  //instance class
+  // name:string='appi';
+  // version:number=11;
+  // instructor:string='hello'
+
+  // courseDetails={
+  //   duration:'2 months',
+  //   modeOfTraining:'online'
+  // }
+  // imgpath:string='https://learn.g2.com/hubfs/What_is_Information_Technology.jpg';
+  // status:boolean=false;
+
+  // data:string;
+  // sendData(d){
+  //   this.data=d;
+  // }
+
+
+
+  // status:boolean=true;
+
+  // arr:number[]=[10,20,30,40,50]
+
+  // changeStatus(){
+  //   this.status=!this.status;
+  // }
+
+  //Assignment
+
