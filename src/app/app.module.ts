ef85167adbe5579995b284c6b5ff1159828b8521
swapnika <telagathotiswapnika9@gmail.com>
2021-05-08 21:53:29 +0530

    'commites'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductsComponent,
+    FooterComponent,
+    ProductDetailsComponent,
     
   ],
   imports: [
